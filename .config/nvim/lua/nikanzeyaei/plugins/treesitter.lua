f60338555610bc41db5ae34a82e17297b8d72074
nikanzeyaei <nikanzeyaei@gmail.com>
2025-03-27 01:31:12 +0330

    feat(nvim): add nvim-treesitter/nvim-treesitter-textobjects

--- .config/nvim/lua/nikanzeyaei/plugins/treesitter.lua
+++ .config/nvim/lua/nikanzeyaei/plugins/treesitter.lua
@@ -2,6 +2,9 @@ return {
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter-textobjects"
+        },
         config = function()
             require("nvim-treesitter.configs").setup({
                 -- A list of parser names, or "all"
