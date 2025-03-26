2f6f5468c9b1e7155bd82fb0cb36b9f5ecf4f215
nikanzeyaei <nikanzeyaei@gmail.com>
2025-03-27 01:28:32 +0330

    feat(nvim): add trouble

---
+++ .config/nvim/lua/nikanzeyaei/plugins/trouble.lua
@@ -0,0 +1,12 @@
+return {
+    "folke/trouble.nvim",
+    opts = {},
+    cmd = "Trouble",
+    keys = {
+        {
+            "<leader>tt",
+            "<cmd>Trouble diagnostics toggle<cr>",
+            desc = "Diagnostics (Trouble)",
+        }
+    },
+}
