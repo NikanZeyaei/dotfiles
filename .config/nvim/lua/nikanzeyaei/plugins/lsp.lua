b85b33dc091a6b6a6b59a780282e7ad28a886148
nikanzeyaei <nikanzeyaei@gmail.com>
2025-04-08 17:15:52 +0330

    fix(nvim): update plugin versions and remove legacy tag from fidget.nvim

--- .config/nvim/lua/nikanzeyaei/plugins/lsp.lua
+++ .config/nvim/lua/nikanzeyaei/plugins/lsp.lua
@@ -2,7 +2,7 @@ return {
 	"neovim/nvim-lspconfig",
 	dependencies = {
 		{ "williamboman/mason.nvim", config = true },
-		{ "j-hui/fidget.nvim", tag = "legacy", opts = {} },
+		{ "j-hui/fidget.nvim", opts = {} },
 		"folke/neodev.nvim",
 		"williamboman/mason-lspconfig.nvim",
 		"hrsh7th/cmp-nvim-lsp",
