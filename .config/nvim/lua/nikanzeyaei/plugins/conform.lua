586d74e05cce063d932a66019aaf8807b831a069
Nikan Zeyaei <nikanzeyaei@Nikans-MacBook-Air.local>
2025-10-22 12:26:16 +0330

    feat(nvim): add eslint_d to conform

--- .config/nvim/lua/nikanzeyaei/plugins/conform.lua
+++ .config/nvim/lua/nikanzeyaei/plugins/conform.lua
@@ -1,25 +1,30 @@
 return {
-	"stevearc/conform.nvim",
-	opts = {
-		formatters_by_ft = {
-			lua = { "stylua", lsp_format = "fallback" },
-			-- javascript = { "eslint_d", "prettierd", stop_after_first = true },
-			typescript = { "biome", lsp_format = "fallback" },
-			-- javascriptreact = { "eslint_d", "prettierd", stop_after_first = true },
-			-- typescriptreact = { "eslint_d", "prettierd", stop_after_first = true },
-			go = { "gofumpt", "golines", "goimports" },
-			c = { "clang-format" },
-			sql = { "sqlfmt" },
-		},
-	},
-	keys = {
-		{
-			"<leader>cf",
-			function()
-				local conform = require("conform")
-				conform.format()
-			end,
-			desc = "Format file",
-		},
-	},
+    "stevearc/conform.nvim",
+    opts = {
+        formatters_by_ft = {
+            lua = { "stylua", lsp_format = "fallback" },
+            -- javascript = { "eslint_d", "prettierd", stop_after_first = true },
+            typescript = { "biome", lsp_format = "fallback" },
+            -- javascriptreact = { "eslint_d", "prettierd", stop_after_first = true },
+            -- typescriptreact = { "eslint_d", "prettierd", stop_after_first = true },
+            go = { "gofumpt", "golines", "goimports" },
+            c = { "clang-format" },
+            sql = { "sqlfmt" },
+        },
+    },
+    keys = {
+        {
+            "<leader>cf",
+            function()
+                local conform = require("conform")
+                conform.format()
+            end,
+            desc = "Format file",
+        },
+        {
+            "<leader>ce",
+            function() require("conform").format({ formatters = { "eslint_d" } }) end,
+            desc = "ESLint fix all (eslint_d)",
+        },
+    },
 }
